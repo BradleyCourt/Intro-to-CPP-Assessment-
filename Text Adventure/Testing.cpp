c4103eb55dd6434f3469064035a98b339abd55f8
Bradley Court <bradley.court@ad.aie.edu>
2016-03-21 15:46:35 +1030

    Removed useless code with testing strings in the text adventure class, finished all testing complete with a percentage, created a new log to display completed date

--- Text Adventure/Testing.cpp
+++ Text Adventure/Testing.cpp
@@ -3,20 +3,24 @@
 #include "Text Adventure.h"
 #include <iostream>
 using namespace std;
-int testNumber;
-int success;
+int testNumber = 0;
+int success = 0;
 void testing(bool test, char* function, fstream& file)
 {
 	++testNumber;
 	if (test)
 	{
 		success++;
-		file << "test " << testNumber << "\t" << function << "\t" << "\success\n";
+		file << "test " << testNumber << "\t" << function << "\t" << "success ";
 	}
 	else
 	{
-		file << "test " << testNumber << "\t" << function << "\tfailed\n";
+		file << "test " << testNumber << "\t" << function << "\tfailed ";
 	}
+
+	float successRate = (float)success / testNumber * 100;
+
+	file << successRate << " % \n";
 }
 
 void test()
@@ -25,36 +29,36 @@ void test()
 	file.open("testlog.txt", ios::app);
 
 	String test1 = "testing";
-	testing(test1.length() == 7, "length", file); // Length
+	testing(test1.length() == 7, "length\t\t", file); // Length
 
 	String test2 = "testing";
-	testing(test2.getCharacter(5) == 'n', "character", file); // Character
+	testing(test2.getCharacter(5) == 'n', "character\t", file); // Character
 
 	String test3 = "testing";
 	test3.compare("testing");
-	testing(strcmp(test3.getString(), "testing") == 0, "compare", file); // Compare
+	testing(strcmp(test3.getString(), "testing") == 0, "compare\t\t", file); // Compare
 
 	String test4 = "testing";
 	test4.append("anything");
-	testing(strcmp(test4.getString(), "testinganything") == 0, "append", file); // Append
+	testing(strcmp(test4.getString(), "testinganything") == 0, "append\t\t", file); // Append
 
 	String test5 = "testing";
 	test5.prepend("anything");
-	testing(strcmp(test5.getString(), "anythingtesting") == 0, "prepend", file); // Prepend
+	testing(strcmp(test5.getString(), "anythingtesting") == 0, "prepend\t\t", file); // Prepend
 
 	String test6 = "testing";
-	testing(strcmp(test6.getString(), "testing") == 0, "getString", file); // Get String
+	testing(strcmp(test6.getString(), "testing") == 0, "getString\t", file); // Get String
 
 	String test7 = "TeStInG";
 	test7.lowerCase();
-	testing(strcmp(test7.getString(), "testing") == 0, "lowercase", file); // Lowercase
+	testing(strcmp(test7.getString(), "testing") == 0, "lowercase\t", file); // Lowercase
 
 	String test8 = "TeStInG";
 	test8.upperCase();
-	testing(strcmp(test8.getString(), "TESTING") == 0, "uppercase", file); // Uppercase
+	testing(strcmp(test8.getString(), "TESTING") == 0, "uppercase\t", file); // Uppercase
 
 	String test9 = "testing";
-	testing(test9.subStringCheck("est") == 1, "subStringCheck", file); // SubStringCheck
+	testing(test9.subStringCheck("est") == 1, "subStringCheck\t", file); // SubStringCheck
 
 	String test10 = "testing"; 
 	String test101 = "esting";
@@ -62,13 +66,14 @@ void test()
 
 	String test11 = "testing";
 	test11.stringReplace("ing", "die");
-	testing(strcmp(test11.getString(), "testdie") == 0, "stringReplace", file); // StringReplace
+	testing(strcmp(test11.getString(), "testdie") == 0, "stringReplace\t", file); // StringReplace
 
 	String test12 = "a";
 	cout << "type a in 'testing'";
 	test12.setString();
-	testing(strcmp(test12.getString(), "testing") == 0, "stringReplace", file); // SetString
+	testing(strcmp(test12.getString(), "testing") == 0, "SetString\t", file); // SetString
 	
+
 }
 
 
